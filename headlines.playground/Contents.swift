980e0aab22221ea43a1ba9cbf7ef460e91baf743
Nick Theodoridis <122683142+nicktheodoridisiOS@users.noreply.github.com>
2024-05-15 20:05:34 +0300

    Upload Playground

---
+++ headlines.playground/Contents.swift
@@ -0,0 +1,3 @@
+import UIKit
+
+var greeting = "The reason this particular project doesn't have source code is because I want it to remain private. However, I still want to present my work through this README file. For any questions or inquiries, feel free to contact me. You can find contact details below."
